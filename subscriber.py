c1d3bfd471130631deef20d54c0dee1fafa749c3
Arda Altıntaş <73823004+ardanyan@users.noreply.github.com>
2022-09-27 19:13:22 +0300

    Add files via upload

---
+++ subscriber.py
@@ -0,0 +1,50 @@
+import paho.mqtt.client as mqtt
+import json
+from datetime import datetime
+
+HOST = "192.168.43.114" # Host Raspberry IP Address
+TOPIC = "sensor_data"
+FORMAT = "utf-8"
+
+
+def logger_callback(msg:str) -> None:
+    print("[SUBSCRIBER] {} - {}".format(
+        datetime.now().strftime("%y/%m/%d %H:%M:%S.%f"), msg))
+
+def on_message_callback(client, userdata, message):
+    logger_callback("Received message: {}".format(
+        json.loads(message.payload.decode(FORMAT))))
+
+
+class Subscriber:
+    def __init__(self, client_id:str) -> None:
+        self.__client = mqtt.Client(client_id)
+        self.__logger = logger_callback
+        self.__client.on_connect = None
+        self.__client.on_message = on_message_callback
+        
+    def start(self, host:str=HOST) -> None:
+        self.__client.connect(host)
+        self.__logger("Started")
+    
+    def subscribe(self, topic:str) -> None:
+        self.__client.loop_start() # Activates callbacks
+        self.__client.subscribe(topic)
+        self.__logger("Subscribed to the topic \'{}\'".format(topic))
+        
+    def close(self) -> None:
+        self.__client.loop_stop() # Deactivate callbacks
+        self.__client.disconnect()
+        self.__logger("Disconnected")
+
+
+if __name__ == "__main__":
+    subscriber_client = Subscriber("SWAIN_Subscriber")
+    subscriber_client.start() # Default parameters
+    subscriber_client.subscribe(TOPIC)
+    while True:
+        try: continue
+        except KeyboardInterrupt:
+            subscriber_client.close()
+            break
+    exit()
